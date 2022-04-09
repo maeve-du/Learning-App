d4584c48935da4deb2f9d634b9a0b661577cd29d
maeve-du <98177760+maeve-du@users.noreply.github.com>
2022-04-10 00:53:09 +0800

    Initial Commit

---
+++ Learning App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning App
+//
+//  Created by Mae on 2022/4/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Mae on 2022/4/10.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

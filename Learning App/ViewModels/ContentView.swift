231dcda6b10bc842c3569c46f336629b0c6705a5
maeve-du <98177760+maeve-du@users.noreply.github.com>
2022-04-10 18:55:50 +0800

    - add assets - add data files - add model - parsed json data into content model - parsed style data into content model

--- Learning App/ViewModels/ContentView.swift
+++ Learning App/ViewModels/ContentView.swift
@@ -6,7 +6,46 @@
 //
 
 import Foundation
+import CoreImage
 
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        // get a url to the  json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // read the file into data object
+            let jsonData = try Data(contentsOf: jsonUrl! )
+            
+            // try to decode the json onto an array of module
+            // and assign parsed modules to modules property
+            let jsonDecoder = JSONDecoder()
+            self.modules =  try jsonDecoder.decode([Module].self, from: jsonData)
+        }
+        catch {
+            print("Couldn't parse local data")
+        }
+        
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            self.styleData  = try Data(contentsOf: styleUrl!)
+
+        }
+        catch {
+            print("Couldn't parse style data")
+        }
+        
+    }
     
 }
+
